01463976450d24bbb7f2eb2fc0a6d3712e42a33a
Parul-Sh <57749861+Parul-Sh@users.noreply.github.com>
2019-12-16 14:26:56 +0530

    Update Table.java

--- Hashtable/src/Table.java
+++ Hashtable/src/Table.java
@@ -3,10 +3,10 @@ public class Table {
 	public static void main(String[] args) {
 		        Hashtable<Integer, String>  hm = new Hashtable<Integer, String>(); 
 		  
-		        hm.put(1, "Geeks"); 
-		        hm.put(12, "forGeeks"); 
-		        hm.put(15, "A computer"); 
-		        hm.put(3, "Portal"); 
+		        hm.put(1, "Grow"); 
+		        hm.put(12, "your"); 
+		        hm.put(15, "computer"); 
+		        hm.put(3, "world"); 
 		  
 		        System.out.println(hm); 
 		    } 
